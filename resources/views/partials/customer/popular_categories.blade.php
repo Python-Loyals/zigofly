6a7f842c61372745febed0118e4b35bdf328bbbd
james-muriithi <muriithijames556@gmail.com>
2020-12-15 11:14:26 +0300

    redirect popular categories

--- resources/views/partials/customer/popular_categories.blade.php
+++ resources/views/partials/customer/popular_categories.blade.php
@@ -12,29 +12,29 @@
             <div class="row ">
                 <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
                     <!-- popular-card-Item -->
-                    <img class="popular-img" src="{{asset('account/images/banners/woman.png')}}">
+                    <img class="popular-img" src="{{asset('account/images/banners/woman.png')}}" data-href="{{route('products.category.search', 'women')}}">
                     <p class="popular-title">Women Clothing</p>
 
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'men')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/man.png')}}">
                     <p class="popular-title">Men Clothing</p>
 
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'toys')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/toys.png')}}">
                     <p class="popular-title">Toys</p>
 
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'electronics')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/electronics.png')}}">
                     <p class="popular-title">Electronics</p>
 
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'beauty')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/cosmetics.png')}}">
                     <p class="popular-title">Beauty</p>
@@ -44,39 +44,39 @@
             </div><br>
 
             <div class="row ">
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'fitness')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/weightlift.png')}}">
                     <p class="popular-title">Fitness</p>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'home & garden')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/pot.png')}}">
                     <p class="popular-title">Home & Garden</p>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'party')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/confetti.png')}}">
                     <p class="popular-title">Party Supplies</p>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'baby & mom')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/mother.png')}}">
                     <p class="popular-title">Baby & Mom</p>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'gifts')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/gift.png')}}">
                     <p class="popular-title">Gifts</p>
                 </div>
             </div><br>
             <div class="row">
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'books')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/book.png')}}">
                     <p class="popular-title">Books</p>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card">
+                <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4 popular-card" data-href="{{route('products.category.search', 'automotives')}}">
                     <!-- popular-card-Item -->
                     <img class="popular-img" src="{{asset('account/images/banners/car.png')}}">
                     <p class="popular-title">Automotives</p>
