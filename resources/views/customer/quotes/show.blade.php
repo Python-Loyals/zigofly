0376d4b9bcbade32ec3fb47449ad4818417718f3
James Muriithi <muriithijames556@gmail.com>
2021-02-02 09:47:13 +0300

    order show pay modal

--- resources/views/customer/quotes/show.blade.php
+++ resources/views/customer/quotes/show.blade.php
@@ -2,6 +2,33 @@
 @section('styles')
     <link rel="stylesheet" href="{{asset('account/css/zigo.css')}}">
     <link rel="stylesheet" href="{{asset('account/css/quote.css')}}">
+    <style>
+        /*spin loader*/
+        #loader {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            z-index: 1;
+            margin: -95px 0 0 -75px;
+            border: 16px solid #f3f3f3;
+            border-radius: 50%;
+            border-top: 16px solid #072448;
+            width: 120px;
+            height: 120px;
+            -webkit-animation: spin 2s linear infinite;
+            animation: spin 2s linear infinite;
+        }
+
+        @-webkit-keyframes spin {
+            0% { -webkit-transform: rotate(0deg); }
+            100% { -webkit-transform: rotate(360deg); }
+        }
+
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+        }
+    </style>
 @endsection
 @section('content')
     <div class="main-content">
